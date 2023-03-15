ca56d12862e0c81535d9972386d74dc7c201ebf9
KAKAOVX <>
2023-03-15 16:18:15 +0900

    춘식 페이지 경로 변경

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import injectHTML from 'vite-plugin-html-inject';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { resolve } from 'path';
 // import { ViteFaviconsPlugin } from "vite-plugin-favicon";
+
 
 const path = require('path');
 
@@ -12,13 +15,15 @@ export default defineConfig({
       '@parts': path.resolve(__dirname, './src/sass/partials'),
       '@styles': path.resolve(__dirname, './src/sass'),
       '@': path.resolve(__dirname, './'),
-      '~': path.resolve(__dirname, './')
-    }
+      '~': path.resolve(__dirname, './'),
+    },
   },
   plugins: [
     injectHTML(),    
+    tsconfigPaths(),
     // ViteFaviconsPlugin({
     //   logo: "./src/assets/favicon.png",
     // }),
   ],
 });
+

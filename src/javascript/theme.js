a1733ec0f1674b37151d21a6f248bb9ed4dfccb4
KAKAOVX <>
2023-03-15 16:55:16 +0900

    페이지 title 수정

--- src/javascript/theme.js
+++ src/javascript/theme.js
@@ -1,14 +1,18 @@
 const themeSwitcher = () => {
+  let modeText = document.querySelector(".mode-text");
 
   window.addEventListener('load', function(){
     document.documentElement.setAttribute('data-theme', 'light');
+    modeText.innerHTML = "라이트 <br> 모드"
   });
 
   document.querySelector('#setMode').addEventListener('change', function(event){
       if(event.target.checked){
         document.documentElement.setAttribute('data-theme', 'dark');
+        modeText.innerHTML = "다크 <br> 모드"
       } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        modeText.innerHTML = "라이트 <br> 모드"
       }
   })
 }

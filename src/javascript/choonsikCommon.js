ae69ca906f0c64b8d418573f2ca782094fcbe2b1
KAKAOVX <>
2023-05-10 17:10:55 +0900

    춘식 다이어리 페이지 좌우 기능 추가

--- src/javascript/choonsikCommon.js
+++ src/javascript/choonsikCommon.js
@@ -2,15 +2,21 @@ const choonsik = () => {
     // gnb
     const barMenu = document.querySelector(".barMenu");
     const gnbWrapper = document.querySelector(".gnb-wrapper");
+    const btnPrev = document.querySelector(".choonsik-condition-btn.btn-prev");
+    const btnNext = document.querySelector(".choonsik-condition-btn.btn-next");
 
     const closeGnb = document.querySelector(".closeGnb");
 
     barMenu.addEventListener("click", () => {
         gnbWrapper.style.display = "block";
+        btnPrev.style.display = "none";
+        btnNext.style.display = "none";
     })
 
     closeGnb.addEventListener("click", () => {
-        gnbWrapper.style.display = "none";
+        gnbWrapper.style.display = "none";    
+        btnPrev.style.display = "block";
+        btnNext.style.display = "block";
     })
 }
 

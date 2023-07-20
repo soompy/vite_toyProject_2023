fdd5a5daf4bb98f0ffddd98a31f045841384580e
KAKAOVX <>
2023-07-20 17:24:50 +0900

    메인페이지 스와이퍼 추가

--- vite.config.js
+++ vite.config.js
@@ -16,18 +16,18 @@ export default defineConfig({
       '@scripts': path.resolve(__dirname, './src/javascript'),
       '@parts': path.resolve(__dirname, './src/sass/partials'),
       '@styles': path.resolve(__dirname, './src/sass'),
-      '@images': path.resolve(__dirname, 'src/assets/images'),      
+      '@images': path.resolve(__dirname, 'src/assets/images'),
       '@': path.resolve(__dirname, './'),
       '~': path.resolve(__dirname, './'),
     },
   },
   plugins: [
-    injectHTML(),    
+    injectHTML(),
     tsconfigPaths(),
     requireTransform({}),
     // ViteFaviconsPlugin({
     //   logo: "./src/assets/favicon.png",
     // }),
-  ],  
+  ],
 });
 

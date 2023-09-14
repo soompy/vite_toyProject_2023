d768cec1d0546a84685fc3c0e677a7f83a616869
KAKAOVX <>
2023-09-14 16:01:44 +0900

    메인 개편

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 import '@styles/main.scss';
 import themeSwitcher from '@scripts/theme.js';
-// import calculations from '@scripts/calculations'
 import choonsikCommon from '@scripts/choonsikCommon';
 import anime from 'animejs/lib/anime.es.js';
 
 themeSwitcher();
-// calculations();
 choonsikCommon();
 anime();
 
--- src/javascript/choonsikCommon.js
+++ src/javascript/choonsikCommon.js
@@ -1,23 +1,56 @@
 const choonsik = () => {
-    // gnb
     const barMenu = document.querySelector(".barMenu");
     const gnbWrapper = document.querySelector(".gnb-wrapper");
     const btnPrev = document.querySelector(".choonsik-condition-btn.btn-go-first");
     const btnNext = document.querySelector(".choonsik-condition-btn.btn-next");
-
     const closeGnb = document.querySelector(".closeGnb");
 
     barMenu.addEventListener("click", () => {
         gnbWrapper.style.display = "block";
         btnPrev.style.display = "none";
         btnNext.style.display = "none";
-    })
-
+    });
     closeGnb.addEventListener("click", () => {
         gnbWrapper.style.display = "none";    
         btnPrev.style.display = "block";
         btnNext.style.display = "block";
-    })
+    });
+
+
+    function disableResizing() {
+        const images = document.querySelectorAll('img');
+        images.forEach((image) => {
+            image.style.maxWidth = '100%';
+            image.style.height = 'auto';
+        });    
+
+        const wrappers = document.querySelector(body);
+        wrappers.forEach((wrapper) => {
+          wrapper.style.maxWidth = '100%';
+          wrapper.style.height = 'auto';
+        });
+    }
+    window.addEventListener('load', disableResizing);
+
+
+    let maxScrollValue;
+    function resizeHandler() {
+        maxScrollValue = document.body.offsetHeight - window.innerHeight;
+    }
+    window.addEventListener('resize', resizeHandler);
+    resizeHandler();
+
+
+    const popupDim = document.querySelector(".dimm");
+    const alertPopup = document.querySelector(".alert-popup");
+    
+    popupDim.classList.add("show");               
+    alertPopup.classList.add("show");   
+    
+    setTimeout(() => {
+        popupDim.classList.remove("show");
+        alertPopup.classList.remove("show");
+    }, 3000);
 }
 
 
